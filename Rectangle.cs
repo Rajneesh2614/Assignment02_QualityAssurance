1b097338d87bb02cac6d256cb7faac6dab71d445
rajneesh.kapoor2614@gmail.com <rajneesh.kapoor2614@gmail.com>
2021-10-01 16:31:17 -0400

    Completed Rectangle Class

---
+++ Rectangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02_QA
+{
+    public class Rectangle
+    {
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public int Lenght { get; set; }
+
+        public Rectangle()
+        {
+            Height = Lenght = Width = 1;
+        }
+
+        public Rectangle(int height, int length, int width)
+        {
+            Height = height;
+            Lenght = length;
+            Width = width;
+        }
+
+        public int GetLength()
+        {
+            return Lenght;
+        }
+
+        public int SetLength(int length)
+        {
+            Lenght = length;
+            return Lenght;
+        }
+
+        public int GetWidth()
+        {
+            return Width;
+        }
+
+        public int SetWidth(int width)
+        {
+            Width = width;
+            return Width;
+        }
+
+        public int SetHeight()
+        {
+            return Height;
+        }
+
+        public int SetHeight(int height)
+        {
+            Height = height;
+            return Height;
+        }
+
+        public int GetVolume()
+        {
+            return Lenght * Height * Width;
+        }
+    }
+}
